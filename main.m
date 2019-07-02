a3591f7d6cfb3be20bf01bf6082bed951a133b4b
Damien LaRocque <phicoltan@gmail.com>
2019-07-02 10:27:32 -0400

    Change 'origine' table to values dict, scatter and axis limits at the end

--- main.m
+++ main.m
@@ -34,10 +34,12 @@ end
 n = numel(contents{1});
 
 % Origine du tracé
-origine = [0, 0]
+values.('x') = [0];
+values.('y') = [0];
 
 % Création de la figure et maintien du tracé
 figure
+grid on
 hold on
 
 for i=1:n
@@ -46,12 +48,39 @@ for i=1:n
     larg = contents{2}(i);
     ang = contents{3}(i);
     
-    origine = tracer_membre(i, origine(1), origine(2), long, larg, ang);
+    fin_pnts = tracer_membre(i, values.('x')(end), values.('y')(end), long, larg, ang);
+    values.('x') = [values.('x'), fin_pnts(1)];
+    values.('y') = [values.('y'), fin_pnts(2)];
 
-    scatter(origine(1), origine(2), 'r')
 end
 
-scatter(origine(1), origine(2), 'g')
+% TODO: Axes
+
+% Dessin des axes
+
+% Fonction pour dessiner des flèches
+drawArrow = @(x,y, varargin) quiver( x(1),y(1),x(2)-x(1),y(2)-y(1),0, varargin{:} );
+
+% Valeurs minimales des axes
+ax_x_max = round(max(values.('x')), -1) + 10;
+ax_y_max = round(max(values.('y')), -1) + 10;
+
+% % Valeurs maximales des axes
+ax_x_min = round(min(values.('x')), -1) - 10;
+ax_y_min = round(min(values.('y')), -1) - 10;
+
+% % Dessin de la flèche d'axe
+% drawArrow([0, ax_x_max], [0, 0], 'MaxHeadSize',0.8, 'linewidth',3,'color','r')
+% drawArrow([0, 0], [0, ax_y_max], 'MaxHeadSize',0.8, 'linewidth',3,'color','r')
 
 
+% Limites d'axe
+xlim([ax_x_min, ax_x_max])
+ylim([ax_y_min, ax_y_max])
+
+
+% Joints du manipulateur sériel
+scatter(values.('x')(1), values.('y')(1), 'k', 'filled')
+scatter(values.('x')(2:end-1), values.('y')(2:end-1), 'r', 'filled')
+scatter(values.('x')(end), values.('y')(end), 'g', 'filled')
 
